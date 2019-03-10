f516d27c1f2d345052ea3575d1f37a5368ea0e04
mohammedsalah19 <themsm1922011@yahoo.com>
2019-03-10 16:20:53 +0200

    second part of classes

--- laywer/Models/Agenda.cs
+++ laywer/Models/Agenda.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,26 @@ namespace laywer.Models
     public class Agenda
     {
         public int id { get; set; }
+        [DisplayName("رقم القضيه")]
+
         public int numberCase { get; set; }
+        [DisplayName("رقم الملف")]
+
         public int FileNumber{ get; set; }
+        [DisplayName("رقم الجلسه")]
+
         public DateTime DateOfGalas { get; set; }
+        [DisplayName("اسم الموكل")]
+ 
         public string ClientName { get; set; }
+        [DisplayName("نوع الموكل")]
+
         public string TypeOfClinet { get; set; }
+        [DisplayName("اسم الخصم")]
+
         public string GuilityName { get; set; }
+        [DisplayName("نوع الخصم")]
+
         public string TypeOfGuility { get; set; }
 
 
---
+++ laywer/Models/documents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace laywer.Models
+{
+    public class documents
+    {
+        public int id { get; set; }
+        public int elgalsaNum { get; set; }
+        public int CaseaNum { get; set; }
+         public string Title { get; set; }
+
+
+    }
+}
